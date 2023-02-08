d5549c1b5f5df093041c028ec2bf79cf1935a027
Steve Storck <steve973@gmail.com>
2023-02-08 15:33:52 -0500

    More reactor improvements.

--- src/main/java/org/storck/filelocator/service/FileEntriesProcessor.java
+++ src/main/java/org/storck/filelocator/service/FileEntriesProcessor.java
@@ -1,6 +1,5 @@
 package org.storck.filelocator.service;
 
-import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.storck.filelocator.model.FileEntry;
@@ -11,19 +10,16 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 import static org.storck.filelocator.service.FileSystemTraverser.ROOT_PARENT;
 
 @Slf4j
 @Service
 public class FileEntriesProcessor {
-
-    private static final String ROOT_DIR = "<SLASH_ROOT>";
 
     final FileEntryRepository fileEntryRepository;
 
@@ -34,66 +30,52 @@ public class FileEntriesProcessor {
         this.fileRelationRepository = fileRelationRepository;
     }
 
-    private static final BiFunction<FileEntry, FileEntryRepository, Mono<Relation>> createEdge = (fileEntry, repo) -> {
+    private static final BiFunction<FileEntry, FileEntryRepository, Relation> createEdge = (fileEntry, repo) -> {
         Relation result = new Relation();
-        try {
-            String fileEntryPath = fileEntry.getPath();
-            if (fileEntryPath.equals(ROOT_PARENT)) {
-                // since there is no parent for the root directory, skip this entry
-                return Mono.just(new Relation());
+        String fileEntryPath = fileEntry.getPath();
+        String parentPath;
+        String parentName;
+        // There is no need to process the root node, since it has no parent
+        if (!fileEntryPath.equals(ROOT_PARENT)) {
+            try {
+                if (fileEntryPath.equals("/")) {
+                    // When the parent directory of a node is the root directory "/", we need to adjust for
+                    // this special case.  When the file system nodes were ingested, the root node was created
+                    // with its nonexistent parent set to the value of the ROOT_PARENT constant, and its name
+                    // value set to "/".
+                    parentPath = ROOT_PARENT;
+                    parentName = "/";
+                } else {
+                    int lastDelimAt = fileEntryPath.lastIndexOf('/');
+                    parentPath = fileEntryPath.substring(0, Math.max(lastDelimAt, 1));
+                    parentName = fileEntryPath.substring(lastDelimAt + 1);
+                }
+                result = Optional.ofNullable(repo.findOneByPathIsAndNameIs(parentPath, parentName))
+                        .map(fe -> new Relation(fe, fileEntry))
+                        .orElseGet(() -> {
+                            log.info("Could not create a parent relationship to fileEntryPath: {}, fileEntryName: {}",
+                                    fileEntryPath, fileEntry.getName());
+                            return new Relation();
+                        });
+            } catch (Exception e) {
+                log.warn("Error when trying to create file system relationship: {}", e.getLocalizedMessage());
             }
-            // Splitting a string when the delimiter is the first character causes a problem of
-            // the first element of the array being empty, so we can solve this issue by prepending
-            // a known string.
-            String[] pathSegments = (ROOT_DIR + fileEntryPath).split("/");
-            int numberOfSegments = pathSegments.length;
-            String path = Arrays.stream(pathSegments, 0, numberOfSegments - 1)
-                    .collect(Collectors.joining("/"));
-            // We have to remove the prepended string before attempting to use it.
-            path = path.replaceFirst(ROOT_DIR, "");
-            // In cases where this is an element in the root directory, there is only one path element,
-            // and we have to remove the prepended string .
-            String name = pathSegments[numberOfSegments - 1].replaceFirst(ROOT_DIR, "");
-            if (Strings.isNullOrEmpty(path) && Strings.isNullOrEmpty(name)) {
-                // The parent directory of an item in the root directory has an empty parent path, and
-                // a name of "/", so we need to adjust this to the special case of using the ROOT_PARENT
-                // constant during ingest of the file system as a placeholder for the parent of the root
-                // directory.  This special case also has the value set to "/" for the name of the root
-                // directory.
-                path = ROOT_PARENT;
-                name = "/";
-            } else if (!path.startsWith("/")) {
-                // Ensure that, for all other cases, the path starts with a forward slash.
-                path = "/" + path;
-            }
-            result = Optional.ofNullable(repo.findOneByPathIsAndNameIs(path, name))
-                    .map(fe -> new Relation(fe, fileEntry))
-                    .orElseGet(() -> {
-                        log.info("Could not create a parent relationship to fileEntryPath: {}, fileEntryName: {}",
-                                fileEntryPath, fileEntry.getName());
-                        return new Relation();
-                    });
-        } catch (Exception e) {
-            log.warn("Error when trying to create file system relationship: {}", e.getLocalizedMessage());
         }
-        return Mono.just(result);
+        return result;
     };
 
-    private static final BiFunction<Collection<Relation>, FileRelationRepository, Mono<Void>> saveEdges = (edges, repo) -> {
+    private static final BiConsumer<Collection<Relation>, FileRelationRepository> saveEdges = (edges, repo) -> {
         repo.saveAll(edges);
-        return Mono.empty();
+        log.info("Batch complete");
     };
 
     public void processForRelationships() {
         Flux.fromIterable(fileEntryRepository.findAll())
-                .parallel()
-                .runOn(Schedulers.boundedElastic())
-                .flatMap(fe -> createEdge.apply(fe, fileEntryRepository))
-                .filter(r -> r.getFrom() != null && r.getTo() != null)
-                .sequential()
+                .flatMap(fe -> Mono.fromCallable(() -> createEdge.apply(fe, fileEntryRepository))
+                        .subscribeOn(Schedulers.boundedElastic())
+                        .filter(r -> r.getFrom() != null && r.getTo() != null))
                 .buffer(1000)
-                .handle((relations, synchronousSink) -> saveEdges.apply(relations, fileRelationRepository))
-                .doOnNext(it -> log.info("Batch complete"))
+                .handle((relations, synchronousSink) -> saveEdges.accept(relations, fileRelationRepository))
                 .blockLast();
     }
 }
--- src/test/java/org/storck/filelocator/FileLocatorApplicationTests.java
+++ src/test/java/org/storck/filelocator/FileLocatorApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FileLocatorApplicationTests {
 
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(FileLocatorApplicationTests.class);
+
 	@Test
 	void contextLoads() {
 	}
-
 }
